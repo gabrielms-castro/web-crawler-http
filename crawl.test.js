2af7494189a618fc73cf999fc748ae854ce599ee
gabrielms-castro <gabrielms.castro@gmail.com>
2025-08-03 20:58:21 -0300

    feat: normalizeURL function and tests for it

---
+++ crawl.test.js
@@ -0,0 +1,42 @@
+const { normalizeURL } = require('./crawl.js')
+const { test, expect } = require('@jest/globals')
+
+test(
+    'normalizeURL strip protocol',
+    () => {
+        const input = 'https://blog.boot.dev/path'
+        const actual = normalizeURL(input)
+        const expected = 'blog.boot.dev/path'
+        expect(actual).toEqual(expected)
+    }
+);
+
+test(
+    'normalizeURL strip trailing slash',
+    () => {
+        const input = 'https://blog.boot.dev/path/'
+        const actual = normalizeURL(input)
+        const expected = 'blog.boot.dev/path'
+        expect(actual).toEqual(expected)
+    }
+);
+
+test(
+    'normalizeURL capitals',
+    () => {
+        const input = 'https://blog.BOOT.dEv/path/'
+        const actual = normalizeURL(input)
+        const expected = 'blog.boot.dev/path'
+        expect(actual).toEqual(expected)
+    }
+);
+
+test(
+    'normalizeURL strip HTTP',
+    () => {
+        const input = 'http://blog.boot.dev/path/'
+        const actual = normalizeURL(input)
+        const expected = 'blog.boot.dev/path'
+        expect(actual).toEqual(expected)
+    }
+);
