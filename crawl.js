05ac214a4132414ad3797956a9908c5b30396a70
gabrielms-castro <gabrielms.castro@gmail.com>
2025-08-05 21:40:29 -0300

    feat crawlPage - crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,27 @@
 const { JSDOM } = require('jsdom')
 
+async function crawlPage(currentURL) {
+    console.log(`Crawling: ${currentURL}`);
+
+    try {
+        const resp = await fetch(currentURL);
+        if (resp.status > 399) {
+            console.log(`Error in fetch with status code ${resp.status} on page ${currentURL}`);
+            return;
+        }
+        
+        const contentType = resp.headers.get('content-type');
+        if (!contentType.includes('text/html') ) {
+            console.log(`Non HTML response. Content-Type: ${contentType}, on page: ${currentURL}`);
+            return;
+
+        }
+        console.log(`resp text: ${await resp.text()}`);
+    } catch (err) {
+        console.log(`Error in fetch: ${err.message}, on page: ${currentURL}`);
+    }
+}
+
 function getURLfromHTML(htmlBody, baseURL) {
     const urls = [];
     const dom = new JSDOM(htmlBody);
@@ -42,5 +64,6 @@ function normalizeURL(urlString) {
 
 module.exports = {
     normalizeURL,
-    getURLfromHTML
+    getURLfromHTML,
+    crawlPage,
 }
