d6a21ba0c6abd049109f2a832c108d5ff6ddfcc0
gabrielms-castro <gabrielms.castro@gmail.com>
2025-09-15 17:54:19 -0300

    feat: add concurrency and maxPages to crawl

---
+++ src/concurrency.js
@@ -0,0 +1,142 @@
+import pLimit from "p-limit";
+import { normalizeURL, getURLFromHTML } from "./crawl.js";
+
+class ConcurrentCrawler {
+    #baseURL;
+    #pages;
+    #limit;
+    #maxPages;
+    #shouldStop = false;
+    #allTasks = new Set();
+    #abortController = new AbortController();
+    #visited = new Set();
+
+    constructor(baseURL, maxConcurrency, maxPages=100) {
+        this.#baseURL = baseURL;
+        this.#pages = {};
+        this.#limit = pLimit(maxConcurrency);
+        this.#maxPages = Math.max(1, maxPages);
+    }
+
+    #addPageVisit(normalizedURL) {
+        if (this.#shouldStop) return false;
+
+        if (this.#pages[normalizedURL]) {
+            this.#pages[normalizedURL]++;
+        } else {
+            this.#pages[normalizedURL] = 1;
+        }
+
+        if (this.#visited.has(normalizedURL)) {
+            return false;
+        }
+
+        if (this.#visited.size >= this.#maxPages) {
+            this.#shouldStop = true;
+            console.log(`[INFO] Reached max pages limit of ${this.#maxPages}. Stopping crawl.`);
+            this.#abortController.abort();
+            return false;
+        }
+
+        this.#visited.add(normalizedURL)
+        return true;
+    }
+
+    async #getHTML(currentURL) {
+        const { signal } = this.#abortController;
+
+        return await this.#limit(async () => {
+            let res;
+            try {
+                res = await fetch(
+                    currentURL, 
+                    {
+                        headers: { "User-Agent": "Mozilla/5.0" },
+                        signal
+                    }
+                );
+            } catch (err) {
+                if (err?.name === "AbortError") {
+                    throw new Error("Fetch aborted");
+                }
+                throw new Error(`Got Network error: ${err.message}`);
+            }
+
+            if (res.status > 399) {
+                throw new Error(`Got HTTP error: ${res.status} ${res.statusText} - URL: ${currentURL}`);
+            }
+
+            const contentType = res.headers.get("content-type");
+            if (!contentType || !contentType.includes("text/html")) {
+                throw new Error(`Got non-HTML response: ${contentType} - URL: ${currentURL}`);
+            }
+
+            return res.text();
+        });
+    }
+
+    async #crawlPage(currentURL) {
+        if (this.#shouldStop) return;
+
+        const currentURLObj = new URL(currentURL);
+        const baseURLObj = new URL(this.#baseURL);
+
+        if (currentURLObj.hostname !== baseURLObj.hostname) {
+            return;
+        }
+
+        const normalizedURL = normalizeURL(currentURL);
+
+        if (!this.#addPageVisit(normalizedURL)) {
+            return
+        }
+
+        console.log(`[INFO] Crawling: ${currentURL}`);
+        let html = "";
+        try {
+            html = await this.#getHTML(currentURL);
+        } catch (err) {
+            console.log(`[ERROR] ${err.message}`);
+            return;
+        }
+        if (html.toLowerCase().includes("art. 1")) return;
+        if (this.#shouldStop) return;
+
+        const nextURLs = getURLFromHTML(html, currentURL)
+        
+        const crawlPromises = [];
+
+        for (const nextURL of nextURLs) {
+            if (this.#shouldStop) break;
+
+            const task = this.#crawlPage(nextURL)
+            this.#allTasks.add(task);
+            task.finally(() => this.#allTasks.delete(task));
+            crawlPromises.push(task);
+        }
+        await Promise.all(crawlPromises);
+    }
+
+    async crawl() {
+        const rootTask = this.#crawlPage(this.#baseURL);
+        this.#allTasks.add(rootTask);
+        try {
+            await rootTask;
+        } finally {
+            this.#allTasks.delete(rootTask);
+        }
+
+        await Promise.allSettled(Array.from(this.#allTasks));
+        
+        //debug
+        console.log(`[DEBUG] Crawling complete. Found ${Object.keys(this.#pages).length} unique pages.`);
+        console.log(`[DEBUG] Visited ${this.#visited.size} pages.`);
+
+        return this.#pages;
+}
+}
+
+export async function crawlSiteAsync(baseURL, maxConcurrency = 5, maxPages=100) {
+    const crawler = new ConcurrentCrawler(baseURL, maxConcurrency, maxPages);
+    return await crawler.crawl();
+}
--- src/main.js
+++ src/main.js
@@ -1,28 +1,51 @@
+import { crawlSiteAsync } from './concurrency.js';
 import { crawlPage } from './crawl.js';
 import { printReport } from './report.js';
 import { DatabaseManager } from './supabase/database.js';
 import { StorageManager } from './supabase/storage.js';
 
 async function main() {
-    if (process.argv.length < 3) {
-        console.log("No Website provided");
+    if (process.argv.length < 5) {
+        console.log("Not enough arguments provided");
+        console.log(
+        "Usage: node src/main.js <baseURL> <maxConcurrency> <maxPages>",
+        );
         process.exit(1);
     }
-    if (process.argv.length > 3) {
+
+    if (process.argv.length > 5) {
         console.log("Too many arguments provided");
         process.exit(1);
     }
 
     const baseURL = process.argv[2];
-    console.log(`Starting crawl of ${baseURL}`);
+    const maxConcurrency = Number(process.argv[3]);
+    const maxPages = Number(process.argv[4]);
+    
+    if (!Number.isFinite(maxConcurrency) || maxConcurrency <= 0) {
+        console.log("invalid maxConcurrency");
+        process.exit(1);
+    }
+    if (!Number.isFinite(maxPages) || maxPages <= 0) {
+        console.log("invalid maxPages");
+        process.exit(1);
+    }
+    
+    console.log(
+        `[INFO] Starting crawl of: ${baseURL} (concurrency=${maxConcurrency}, maxPages=${maxPages})...`,
+    );
 
-    // Instance of Supabase DB and Supabase Storage
-    // SupabaseClient is instantitated only once in a singleton pattern within client.js
-    const database = new DatabaseManager();
-    const storage = new StorageManager();
-
-    const pages = await crawlPage(baseURL, baseURL, {}, database, storage) //1st baseURL is the starting point, 2nd is the current URL, {} is a new empty object to hold the pages
+    const pages = await crawlSiteAsync(baseURL, maxConcurrency, maxPages);    
     printReport(pages);
-}   
+    
+    process.exit(0);
+}
+// Instance of Supabase DB and Supabase Storage
+// SupabaseClient is instantitated only once in a singleton pattern within client.js
+// const database = new DatabaseManager();
+// const storage = new StorageManager();
+// const pages = await crawlPage(baseURL, baseURL, {}, database, storage) //1st baseURL is the starting point, 2nd is the current URL, {} is a new empty object to hold the pages
+    
+
 
 main();
